5bba908711f8e1f48495f485298b30ac80f6eef2
IgoreSHik <balyko070699@gmail.com>
2022-01-05 02:11:40 +0100

    Zadanie7

--- app/src/main/java/com/example/zadanie6try100500balyka/MainActivity.java
+++ app/src/main/java/com/example/zadanie6try100500balyka/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Button button_newTask;
+    public static TextView tasksLeftCounter;
+
+    public static void setTasksLeftCounter(int x)
+    {
+        tasksLeftCounter.setText(Integer.toString(x));
+    }
 
     String s1[], s2[];
 
@@ -18,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        button_newTask = findViewById(R.id.button_newTask);
 
-        s1 = getResources().getStringArray(R.array.programming_languages);
-        s2 = getResources().getStringArray(R.array.description);
         TaskStorage taskStorage = TaskStorage.takeTaskStorage();
 
         //MyAdapter myAdapter = new MyAdapter(this, s1, s2);
-        MyAdapter myAdapter = new MyAdapter(this, TaskStorage.getNames(), TaskStorage.getIsDone());
+        MyAdapter myAdapter = new MyAdapter(this, TaskStorage.getNames(), TaskStorage.getIsDone(), button_newTask);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksLeftCounter = findViewById(R.id.tasks_left_counter);
     }
 }
--- app/src/main/java/com/example/zadanie6try100500balyka/MyAdapter.java
+++ app/src/main/java/com/example/zadanie6try100500balyka/MyAdapter.java
@@ -2,15 +2,25 @@ package com.example.zadanie6try100500balyka;
 
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.TriggerEventListener;
+import android.location.GnssAntennaInfo;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintsChangedListener;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -18,16 +28,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     String data1[];
     boolean data2[];
     Context context;
+    int itemCount;
+    Button button_newTask;
 
-    public MyAdapter(Context ct, String s[]){
-        context = ct;
-        data1 = s;
-    }
-
-    public MyAdapter(Context ct, String s[], boolean b[]){
+    public MyAdapter(Context ct, String s[], boolean b[], Button button){
         context = ct;
         data1 = s;
         data2 = b;
+        itemCount = 5;
+        button_newTask = button;
+        button_newTask.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemCount++;
+                TasksLeftCount();
+            }
+        });
+    }
+
+    public void TasksLeftCount() {
+        int tasksLeftCounter = 0;
+        for (int i = 0; i < itemCount; i++)
+            if (!data2[i]) tasksLeftCounter++;
+        MainActivity.setTasksLeftCounter(tasksLeftCounter);
     }
 
     @NonNull
@@ -42,6 +65,38 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.myText.setText(data1[position]);
         holder.checkBox.setChecked(data2[position]);
+        if (data2[position]) {
+            holder.imageView_undone.setVisibility(View.INVISIBLE);
+            holder.imageView_done.setVisibility(View.VISIBLE);
+        } else {
+            holder.imageView_undone.setVisibility(View.VISIBLE);
+            holder.imageView_done.setVisibility(View.INVISIBLE);
+        }
+        holder.checkBox.setOnClickListener(new CompoundButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.checkBox.isChecked()) {
+                    holder.imageView_undone.setVisibility(View.INVISIBLE);
+                    holder.imageView_done.setVisibility(View.VISIBLE);
+                    data2[position] = true;
+                } else {
+                    holder.imageView_undone.setVisibility(View.VISIBLE);
+                    holder.imageView_done.setVisibility(View.INVISIBLE);
+                    data2[position] = false;
+                }
+                TasksLeftCount();
+            }
+        });
+        if (position<itemCount) {holder.myLayout.setVisibility(View.VISIBLE);}
+        else {holder.myLayout.setVisibility(View.INVISIBLE);}
+    }
+
+    @Override
+    public void onViewRecycled(@NonNull MyViewHolder holder) {
+        super.onViewRecycled(holder);
+        int position = holder.getAdapterPosition();
+        data1[position] = holder.myText.getText().toString();
+        data2[position] = holder.checkBox.isChecked();
     }
 
     @Override
@@ -53,14 +108,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         TextView myText;
         //ConstraintLayout myLayout;
-        LinearLayout myLayout;
+        ConstraintLayout myLayout;
         CheckBox checkBox;
+        ImageView imageView_done, imageView_undone;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             myText = itemView.findViewById(R.id.task_name);
             checkBox = itemView.findViewById(R.id.task_done);
             myLayout = itemView.findViewById(R.id.linear_layout);
+            imageView_done = itemView.findViewById(R.id.imageView_done);
+            imageView_undone = itemView.findViewById(R.id.imageView_undone);
+            TasksLeftCount();
         }
     }
 }
